cb5fe1a94293bde7c34485c54034d864d9718b98
Alex Scott <alex.scott@ird.govt.nz>
2025-08-06 19:35:29 +1200

    Fix catch all route  location from loading fonts as pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // Exclude static assets from [...location] route
+        {
+          source: '/_next/:path*',
+          destination: '/_next/:path*',
+        },
+        {
+          source: '/favicon.ico',
+          destination: '/favicon.ico',
+        },
+        {
+          source: '/robots.txt', 
+          destination: '/robots.txt',
+        },
+        {
+          source: '/sitemap.xml',
+          destination: '/sitemap.xml',
+        }
+      ]
+    }
+  }
 };
 
 export default nextConfig;
