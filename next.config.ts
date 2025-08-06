ede89a495bbfcf7fbc03d1d85ddc0c425f1e6e3f
Alex Scott <alex.scott@ird.govt.nz>
2025-08-06 20:25:11 +1200

    Test prefetch dynaic in next config

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,13 @@ const nextConfig: NextConfig = {
         }
       ]
     }
-  }
+  },
+  experimental: {
+    staleTimes: {
+      dynamic: 300, // 5 minutes - matches your revalidate = 300
+      static: 300,  // 5 minutes for consistency
+    },
+  },
 };
 
 export default nextConfig;
